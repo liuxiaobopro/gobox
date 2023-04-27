bfebf75d263f1b8081f4b34a422b42a91fae3756
liuxiaobo <isliuxiaobo@gmail.com>
2023-04-28 03:25:09 +0800

    🎈 perf: handle

--- http/handle.go
+++ http/handle.go
@@ -8,44 +8,46 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type GinHandle struct{}
+type GinHandle struct {
+	c *gin.Context
+}
 
 // ShouldBind Get绑定到结构体
-func (*GinHandle) ShouldBind(c *gin.Context, s interface{}) error {
-	return c.ShouldBind(&s)
+func (h *GinHandle) ShouldBind(s interface{}) error {
+	return h.c.ShouldBind(&s)
 }
 
 // ShouldBindJSON Post绑定到结构体
-func (*GinHandle) ShouldBindJSON(c *gin.Context, s interface{}) error {
-	return c.ShouldBindJSON(&s)
+func (h *GinHandle) ShouldBindJSON(s interface{}) error {
+	return h.c.ShouldBindJSON(&s)
 }
 
 // Param 获取路由参数
-func (*GinHandle) Param(c *gin.Context, key string) string {
-	return c.Param(key)
+func (h *GinHandle) Param(key string) string {
+	return h.c.Param(key)
 }
 
 // Query 获取get参数
-func (*GinHandle) Query(c *gin.Context, key string) string {
-	return c.Query(key)
+func (h *GinHandle) Query(key string) string {
+	return h.c.Query(key)
 }
 
 // DefaultQuery 获取get参数，如果没有则返回默认值
-func (*GinHandle) DefaultQuery(c *gin.Context, key string, defaultValue string) string {
-	return c.DefaultQuery(key, defaultValue)
+func (h *GinHandle) DefaultQuery(key string, defaultValue string) string {
+	return h.c.DefaultQuery(key, defaultValue)
 }
 
 // ReturnJSON 返回json
-func (*GinHandle) ReturnJSON(c *gin.Context, code int, data interface{}) {
-	c.JSON(code, data)
+func (h *GinHandle) ReturnJSON(code int, data interface{}) {
+	h.c.JSON(code, data)
 }
 
 // RetuenOk 返回成功json
-func (*GinHandle) RetuenOk(c *gin.Context, data interface{}) {
-	c.JSON(http.StatusOK, respx.Succ(data))
+func (h *GinHandle) RetuenOk(data interface{}) {
+	h.c.JSON(http.StatusOK, respx.Succ(data))
 }
 
 // ReturnErr 返回错误json
-func (*GinHandle) ReturnErr(c *gin.Context, r respx.T) {
-	c.JSON(http.StatusBadRequest, r)
+func (h *GinHandle) ReturnErr(r respx.T) {
+	h.c.JSON(http.StatusBadRequest, r)
 }
