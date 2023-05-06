49d8cac05e736e8664c3a4a65dc5e80c77ce27ec
liuxiaobo <isliuxiaobo@gmail.com>
2023-05-06 11:33:39 +0800

    🎈 perf: ModelExtends 改成下划线

--- types/xorm.go
+++ types/xorm.go
@@ -5,17 +5,17 @@ import (
 )
 
 type ModelExtends struct {
-	CreateAt  timex.JsonTime `xorm:"comment('创建时间') DATETIME created" json:"createAt"`
-	UpdateAt  timex.JsonTime `xorm:"comment('更新时间') DATETIME updated" json:"updateAt"`
-	CreateUid int            `xorm:"comment('创建人id') INT(11) default 0" json:"createUid"`
-	UpdateUid int            `xorm:"comment('更新人id') INT(11) default 0" json:"updateUid"`
+	CreateAt  timex.JsonTime `xorm:"comment('创建时间') DATETIME created" json:"create_at"`
+	UpdateAt  timex.JsonTime `xorm:"comment('更新时间') DATETIME updated" json:"update_at"`
+	CreateUid int            `xorm:"comment('创建人id') INT(11) default 0" json:"create_uid"`
+	UpdateUid int            `xorm:"comment('更新人id') INT(11) default 0" json:"update_uid"`
 }
 
 type ModelCTExtends struct {
-	CreateAt timex.JsonTime `xorm:"comment('创建时间') DATETIME created" json:"createAt"`
+	CreateAt timex.JsonTime `xorm:"comment('创建时间') DATETIME created" json:"create_at"`
 }
 
 type ModelCUExtends struct {
-	CreateAt timex.JsonTime `xorm:"comment('创建时间') DATETIME created" json:"createAt"`
-	UpdateAt timex.JsonTime `xorm:"comment('更新时间') DATETIME updated" json:"updateAt"`
+	CreateAt timex.JsonTime `xorm:"comment('创建时间') DATETIME created" json:"create_at"`
+	UpdateAt timex.JsonTime `xorm:"comment('更新时间') DATETIME updated" json:"update_at"`
 }
