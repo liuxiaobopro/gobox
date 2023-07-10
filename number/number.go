bf55cf9320977df0b7db461ce384c21a3d705bf6
liuxiaobo <isliuxiaobo@gmail.com>
2023-07-10 17:53:20 +0800

    ✨ feat: RandomInt

--- number/number.go
+++ number/number.go
@@ -1,1 +1,12 @@
 package number
+
+import (
+	"math/rand"
+	"time"
+)
+
+// RandomInt 获取随机数(左开右闭)
+func RandomInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return min + rand.Intn(max-min)
+}
