c571c03640b2ceb0e32a6ed0ffc65876f7a90d18
liuxiaobo <isliuxiaobo@gmail.com>
2023-09-21 15:59:21 +0800

    🎈 perf: DecryptCiphertext

--- pay/wechat/refund/types.go
+++ pay/wechat/refund/types.go
@@ -16,3 +16,22 @@ type Notify struct {
 	Summary      string   `json:"summary,omitempty"`
 	Resource     Resource `json:"resource,omitempty"`
 }
+
+type Amount struct {
+	Total       int `json:"total,omitempty"`
+	Refund      int `json:"refund,omitempty"`
+	PayerTotal  int `json:"payer_total,omitempty"`
+	PayerRefund int `json:"payer_refund,omitempty"`
+}
+
+type DecryptCiphertext struct {
+	MchID               string `json:"mchid,omitempty"`
+	TransactionID       string `json:"transaction_id,omitempty"`
+	OutTradeNo          string `json:"out_trade_no,omitempty"`
+	RefundID            string `json:"refund_id,omitempty"`
+	OutRefundNo         string `json:"out_refund_no,omitempty"`
+	RefundStatus        string `json:"refund_status,omitempty"`
+	SuccessTime         string `json:"success_time,omitempty"`
+	UserReceivedAccount string `json:"user_received_account,omitempty"`
+	Amount              Amount `json:"amount,omitempty"`
+}
