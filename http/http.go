e55df1a962f227f80284dcc9bfde4e279198101f
liuxiaobo <isliuxiaobo@gmail.com>
2023-10-10 15:47:50 +0800

    ✨ feat: IsValidUrl

--- http/http.go
+++ http/http.go
@@ -3,9 +3,13 @@ package http
 import (
 	"bytes"
 	"io"
+	"math/rand"
 	"net/http"
+	"net/url"
+	"time"
 
 	"github.com/liuxiaobopro/gobox/mapx"
+	numberx "github.com/liuxiaobopro/gobox/number"
 )
 
 type Client struct {
@@ -81,4 +85,72 @@ func (Client *Client) Post() ([]byte, error) {
 	}
 	defer resp.Body.Close()
 	return io.ReadAll(resp.Body)
+}
+
+// GetRandomUserAgent 获取随机User-Agent
+func GetRandomUserAgent() string {
+	userAgents := []string{
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0",
+		"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
+		"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0",
+		"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36",
+		"Mozilla/5.0 (X11; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0",
+		"Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1",
+		"Mozilla/5.0 (iPad; CPU OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1",
+		"Mozilla/5.0 (Android 10; Mobile; rv:99.0) Gecko/99.0 Firefox/99.0",
+	}
+
+	// 使用当前时间作为随机数生成器的种子
+	rand.Seed(time.Now().UnixNano())
+
+	// 生成随机索引
+	randomIndex := numberx.RandomInt(0, len(userAgents))
+
+	// 返回随机选择的User-Agent字符串
+	return userAgents[randomIndex]
+}
+
+// IsValidUrl 是否是有效的url
+func IsValidUrl(str string) bool {
+	if parsedURL, err := url.Parse(str); err != nil {
+		return false
+	} else {
+		if parsedURL.Scheme == "" || parsedURL.Host == "" {
+			return false
+		}
+
+		url := parsedURL.Scheme + "://" + parsedURL.Host
+
+		headers := make(http.Header)
+		headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)")
+
+		// 创建一个自定义的http.Client
+		client := &http.Client{
+			Timeout: 3 * time.Second, // 设置超时时间
+		}
+
+		// 创建HTTP请求
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		if err != nil {
+			return false
+		}
+
+		req.Header = headers
+
+		// 发送HTTP请求
+		resp, err := client.Do(req)
+		if err != nil {
+			return false
+		}
+
+		if resp.StatusCode >= 500 && resp.StatusCode < 600 {
+			return false
+		} else {
+			return true
+		}
+	}
 }
--- http/http_test.go
+++ http/http_test.go
@@ -19,3 +19,50 @@ func TestClient_Get(t *testing.T) {
 
 func TestClient_Post(t *testing.T) {
 }
+
+func TestIsValidUrl(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "1",
+			args: args{
+				str: "http://www.baidu.com",
+			},
+			want: true,
+		},
+		{
+			name: "2",
+			args: args{
+				str: "https://www.baidu.com",
+			},
+			want: true,
+		},
+		{
+			name: "3",
+			args: args{
+				str: "www.baidu.com",
+			},
+			want: false,
+		},
+		{
+			name: "4",
+			args: args{
+				str: "1dd",
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsValidUrl(tt.args.str); got != tt.want {
+				t.Errorf("IsValidUrl() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
