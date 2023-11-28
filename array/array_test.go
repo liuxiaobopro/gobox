dff052521d7cd875fec8679688ab19a3b09bd92c
liuxiaobo <isliuxiaobo@gmail.com>
2023-11-28 10:40:26 +0800

    ✨ feat: temp

--- array/array_test.go
+++ array/array_test.go
@@ -21,7 +21,7 @@ func TestIntersection(t *testing.T) {
 }
 
 func TestDifference(t *testing.T) {
-	arr1 := []string{"1", "2", "3", "4"}
+	arr1 := []string{"1", "2", "3", "4", "4"}
 	arr2 := []string{"3", "4", "5", "6"}
 	want := []string{"1", "2", "5", "6"}
 	got := Difference(arr1, arr2)
