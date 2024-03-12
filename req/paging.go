92ae728a773673af3e0b582182181cc70618bbaf
liuxiaobo <isliuxiaobo@gmail.com>
2024-03-12 09:17:45 +0800

    🎈 perf: req

--- req/paging.go
+++ req/paging.go
@@ -9,3 +9,8 @@ type ListNoRequiredSize struct {
 	Page int `json:"page" form:"page" uri:"page" binding:"required"`
 	Size int `json:"size" form:"size" uri:"size"`
 }
+
+type ListNoRequired struct {
+	Page int `json:"page" form:"page" uri:"page"`
+	Size int `json:"size" form:"size" uri:"size"`
+}
