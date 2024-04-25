d0affa2d8cc00d4351a9a4d49fadd49aaeea5191
liuxiaobo <isliuxiaobo@gmail.com>
2024-04-25 17:56:19 +0800

    ✨ feat: ListDefault

--- req/paging.go
+++ req/paging.go
@@ -14,3 +14,8 @@ type ListNoRequired struct {
 	Page int `json:"page" form:"page" uri:"page"`
 	Size int `json:"size" form:"size" uri:"size"`
 }
+
+type ListDefault struct {
+	PN int `json:"pn" form:"pn" uri:"pn"`
+	PL int `json:"pl" form:"pl" uri:"pl"`
+}
