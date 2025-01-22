a98a83c9a301a7292e2e408418f3a425bfe27624
liuxiaobo <1369924600@qq.com>
2025-01-22 13:21:35 +0800

    🎈 perf: time func

--- time/time.go
+++ time/time.go
@@ -20,24 +20,40 @@ const (
 // IntToString 将int转换为时间字符串
 // 例如: 1560000000 -> 2019-06-09 00:00:00
 func IntToString(t int) string {
+	if t == 0 {
+		return ""
+	}
+
 	return time.Unix(int64(t), 0).Format(FormatDateTime)
 }
 
 // IntToStringDate 将int转换为日期字符串
 // 例如: 1560000000 -> 2019-06-09
 func IntToStringDate(t int) string {
+	if t == 0 {
+		return ""
+	}
+
 	return time.Unix(int64(t), 0).Format(FormatDate)
 }
 
 // IntToStringTime 将int转换为时间字符串
 // 例如: 1560000000 -> 00:00:00
 func IntToStringTime(t int) string {
+	if t == 0 {
+		return ""
+	}
+
 	return time.Unix(int64(t), 0).Format(FormatTime)
 }
 
 // StringToInt 将时间字符串转换为int
 // 例如: 2019-06-09 00:00:00 -> 1560000000
 func StringToInt(t string) int {
+	if t == "" {
+		return 0
+	}
+
 	loc, err := time.LoadLocation("Local") // 获取时区(中国上海; TimeZoneSH)
 	if err != nil {
 		panic(err)
@@ -52,6 +68,10 @@ func StringToInt(t string) int {
 // StringToTime 将时间字符串转换为time
 // 例如: 2019-06-09 00:00:00 -> time
 func StringToTime(t string) Time {
+	if t == "" {
+		return Time{}
+	}
+
 	loc, err := time.LoadLocation("Local") // 获取时区(中国上海; TimeZoneSH)
 	if err != nil {
 		panic(err)
@@ -66,6 +86,10 @@ func StringToTime(t string) Time {
 // StringToDate 将时间字符串转换为time
 // 例如: 2019-06-09 -> time
 func StringToDate(t string) Time {
+	if t == "" {
+		return Time{}
+	}
+
 	loc, err := time.LoadLocation("Local") // 获取时区(中国上海; TimeZoneSH)
 	if err != nil {
 		panic(err)
